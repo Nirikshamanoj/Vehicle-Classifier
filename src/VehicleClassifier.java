70700635b8c09c0d5aec2c48a396412c117ff4e5
Niriksha M <88819709+Nirikshamanoj@users.noreply.github.com>
2023-07-11 12:08:23 +0530

    Update VehicleClassifier.java

--- src/VehicleClassifier.java
+++ src/VehicleClassifier.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class VehicleClassifier {
+abstract class Vehicle
+{
+    abstract void start();
+    abstract void stop();
+
 }
+class Car extends Vehicle
+{
+    @Override
+    public void start()
+    {
+        System.out.println("Car Started");
+    }
+    @Override
+    public void stop()
+    {
+        System.out.println("Car Stopped");
+    }
+}
+class MotorCycle extends Vehicle
+{
+    @Override
+    public void start()
+    {
+        System.out.println("MotorCycle Started");
+    }
+    @Override
+    public void stop()
+    {
+        System.out.println("MotorCycle Stopped");
+    }
+}
+public class VehicleClassifier
+{
+    public static void main(String[] args) {
+        MotorCycle motorCycle=new MotorCycle();
+        Car car=new Car();
+        car.start();
+        car.stop();
+        motorCycle.start();
+        motorCycle.stop();
+    }
+}
+
